1a98d02cc62a4076c00f8b05bb5b6ea622041c44
DanielGSalcedo <134619621+DanielGSalcedo@users.noreply.github.com>
2023-05-25 10:45:32 -0500

    adding deletreo

--- PrubasGitHub.java
+++ PrubasGitHub.java
@@ -3,5 +3,9 @@ public class PrubasGitHub {
         System.out.println("Hello Welcome to GitHub");
         String name = "Daniel";
         System.out.println("hello "+name);
+
+        for(int i = 0 ; i <= name.lenght;i++){
+            System.out.println(name.chartArt(i))
+        }
     }
 }
