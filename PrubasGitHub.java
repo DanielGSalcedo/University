f9fa4ca53282b20779f49b12b84ff182cd2f127c
DanielGSalcedo <134619621+DanielGSalcedo@users.noreply.github.com>
2023-05-25 10:28:20 -0500

    Update PrubasGitHub.java
    addiing my name

--- PrubasGitHub.java
+++ PrubasGitHub.java
@@ -1,5 +1,7 @@
 public class PrubasGitHub {
     public static void main(String[] args) {
         System.out.println("Hello Welcome to GitHub");
+        String name = "Daniel";
+        System.out.println("hello "+name);
     }
 }
