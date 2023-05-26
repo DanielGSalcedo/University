632a49d895a44ca205ab16b1a6e14d5964830750
DanielGSalcedo <134619621+DanielGSalcedo@users.noreply.github.com>
2023-05-26 16:10:48 -0500

    first previo 2 version 2.0

--- ParqueDelCafe/src/Main.java
+++ ParqueDelCafe/src/Main.java
@@ -2,14 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int atrac , clientes , acro , limitEdad ,ingresos =0, limitAlt , restric = 0, tick = 0;
+        int atrac , clientes , acro , limitEdad , limitAlt , restric = 0, tick = 0,j = 0;
         Scanner scan = new Scanner(System.in);
 
         System.out.println("ingrese el numero de atracciones disponibles");
         atrac = scan.nextInt();
-        System.out.println("cuantos clientes se esperan atender hoy por atracion?");
+        System.out.println("cuantos clientes se esperan atender hoy ?");
         clientes = scan.nextInt();
         scan.nextLine();
+
+        int[]edadCl = new int[clientes];
+        int[]altCl = new int[clientes];
+        int[] doc = new int[clientes];
 
         for(int i = 1; i<=atrac;i++){
             System.out.println("--------------------------------------------------------------");
@@ -19,31 +23,36 @@ public class Main {
             acro = scan.nextInt();
             System.out.println("ingrese la edad minima permitida para su ingreso");
             limitEdad = scan.nextInt();
-            System.out.println("ingrese la altura minima permitida para su ingreso");
+            System.out.println("ingrese la altura minima en cm permitida para su ingreso");
             limitAlt = scan.nextInt();
             System.out.println("--------------------------------------------------------------");
-            do{
+
+          while (j<= clientes-1) {
+                System.out.println("Bienvenido esta en la atracion ".concat(nameAtrac));
                 System.out.println("--------------------------------------------------------------");
-                System.out.println("esta en la atracion "+ i);
-                System.out.println("hola porfavor ingrse su numero de documento");
-                int doc = scan.nextInt();
+                System.out.println("Porfavor ingrese su numero de documento");
+                doc[j]= scan.nextInt();
                 System.out.println("ingrese su altura en cm");
-                int altura = scan.nextInt();
+                altCl[j] = scan.nextInt();
                 System.out.println("ingrese su edad");
-                int edadClient = scan.nextInt();
+                edadCl[j] = scan.nextInt();
                 scan.nextLine();
                 System.out.println("--------------------------------------------------------------");
-                if(limitAlt> altura || limitEdad > edadClient || (acro == 3 && (edadClient < 18 || edadClient > 40))){
-                    System.out.println("no cumple con los requisitos para entrar a la atracion ");
+                j++;
+            }
+            for (int k = 0;k<= clientes-1;k++) {
+                String concat = "cliente con el numero de identificacion :".concat(String.valueOf(doc[k]));
+                if (limitAlt > altCl[k] || limitEdad > edadCl[k] || (acro == 3 && (edadCl[k] < 18 || edadCl[k] > 40))) {
+                    System.out.println(concat);
+                    System.out.println("no cumple con los requisitos para entrar a la atracion : ".concat(nameAtrac));
                     restric++;
-                }else{
-                    ingresos++;
+                } else {
+                    System.out.println(concat);
+                    System.out.println("bienvenido a la atracion su ticket es : ATC" + tick + (i));
                     tick++;
-                    System.out.println("bienvenido a la atracion su ticket es : ATC"+tick+(i));
                 }
                 System.out.println("--------------------------------------------------------------");
-            }while(ingresos < clientes);
-            ingresos = 0;
+            }
         }
         System.out.println("--------------------------------------------------------------");
         System.out.println("numero de tickets entregados en el parque : "+ tick);
