db7dcdbe7ca3db16ed9b92b5d97c18fcd306e2ba
DanielGSalcedo <134619621+DanielGSalcedo@users.noreply.github.com>
2023-05-26 19:07:04 -0500

    first previo 2 version 2.5

--- ParqueDelCafe/src/Main.java
+++ ParqueDelCafe/src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int atrac , clientes , acro , limitEdad , limitAlt , restric = 0, tick = 0,j = 0;
+        int atrac , clientes , acro , limitEdad , limitAlt , restric = 0, tick = 0;
         Scanner scan = new Scanner(System.in);
 
         System.out.println("ingrese el numero de atracciones disponibles");
@@ -19,35 +19,35 @@ public class Main {
             System.out.println("--------------------------------------------------------------");
             System.out.println("ingrese el nombre de la atraccion mecanica");
             String nameAtrac = scan.nextLine();
-            System.out.println("resgistre el nivel de acrofobia 1 bajo, 2 medio, 3 alto");
-            acro = scan.nextInt();
+            do {
+                System.out.println("resgistre el nivel de acrofobia 1 bajo, 2 medio, 3 alto");
+                acro = scan.nextInt();
+            }while (acro<1 || acro >3);
             System.out.println("ingrese la edad minima permitida para su ingreso");
             limitEdad = scan.nextInt();
             System.out.println("ingrese la altura minima en cm permitida para su ingreso");
             limitAlt = scan.nextInt();
             System.out.println("--------------------------------------------------------------");
 
-          while (j<= clientes-1) {
-                System.out.println("Bienvenido esta en la atracion ".concat(nameAtrac));
-                System.out.println("--------------------------------------------------------------");
-                System.out.println("Porfavor ingrese su numero de documento");
-                doc[j]= scan.nextInt();
-                System.out.println("ingrese su altura en cm");
-                altCl[j] = scan.nextInt();
-                System.out.println("ingrese su edad");
-                edadCl[j] = scan.nextInt();
-                scan.nextLine();
-                System.out.println("--------------------------------------------------------------");
-                j++;
-            }
-            for (int k = 0;k<= clientes-1;k++) {
-                String concat = "cliente con el numero de identificacion :".concat(String.valueOf(doc[k]));
-                if (limitAlt > altCl[k] || limitEdad > edadCl[k] || (acro == 3 && (edadCl[k] < 18 || edadCl[k] > 40))) {
-                    System.out.println(concat);
+            for (int j = 0;j< clientes;j++) {
+                if (i == 1) {
+                    System.out.println("Bienvenido esta en la atracion ".concat(nameAtrac));
+                    System.out.println("--------------------------------------------------------------");
+                    System.out.println("Porfavor ingrese su numero de documento");
+                    doc[j] = scan.nextInt();
+                    System.out.println("ingrese su altura en cm");
+                    altCl[j] = scan.nextInt();
+                    System.out.println("ingrese su edad");
+                    edadCl[j] = scan.nextInt();
+                    scan.nextLine();
+                    System.out.println("--------------------------------------------------------------");
+                }
+                String concat = "cliente con el numero de identificacion :".concat(String.valueOf(doc[j]));
+                System.out.println(concat);
+                if (limitAlt > altCl[j] || limitEdad > edadCl[j] || (acro == 3 && (edadCl[j] < 18 || edadCl[j] > 40))) {
                     System.out.println("no cumple con los requisitos para entrar a la atracion : ".concat(nameAtrac));
                     restric++;
                 } else {
-                    System.out.println(concat);
                     System.out.println("bienvenido a la atracion su ticket es : ATC" + tick + (i));
                     tick++;
                 }
